7fce881aa286ea5201583834a03ae275e280b42c
Gianella Xiomara Coronel Manchego <gia.ne12@hotmail.com>
2026-01-22 08:18:54 -0500

    feat: set a basic server

---
+++ server.js
@@ -0,0 +1,12 @@
+import http from "node:http";
+
+const PORT = 8000;
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  res.statusCode = 200;
+  res.end("<html><h1>The server is working</h1></html>");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
