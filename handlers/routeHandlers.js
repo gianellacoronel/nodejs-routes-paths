a55631e25a413a3f4d367c430dff909a2a08cb9d
Gianella Xiomara Coronel Manchego <gia.ne12@hotmail.com>
2026-01-25 08:48:13 -0500

    feat: add an Event Emitter

--- handlers/routeHandlers.js
+++ handlers/routeHandlers.js
@@ -1,3 +1,4 @@
+import { sightingEvents } from "../events/sightingEvents.js";
 import { addNewSighting } from "../utils/addNewSighting.js";
 import { getData } from "../utils/getData.js";
 import { parseJSONBody } from "../utils/parseJSONBody.js";
@@ -15,6 +16,17 @@ export async function handlePost(req, res) {
     const parsedBody = await parseJSONBody(req);
     const sanitizedBody = sanitizeInput(parsedBody);
     await addNewSighting(sanitizedBody);
+
+    /*
+    Challenge 2
+    1. At the top of this file, import the event emitter you have created.
+    2. Use it to emit a ‘sighting-added’ event.
+       What information does the listener function need?
+    3. Add a sighting to test!
+    */
+
+    sightingEvents.emit("sighting-added", sanitizedBody);
+
     sendResponse(res, JSON.stringify(parsedBody), 201, "application/json");
   } catch (err) {
     sendResponse(res, JSON.stringify({ error: err }), 400, "application/json");
