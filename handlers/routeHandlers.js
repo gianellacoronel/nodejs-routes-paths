9a0e45349131e01ed20f01f47266a28219826ce8
Gianella Xiomara Coronel Manchego <gia.ne12@hotmail.com>
2026-01-25 10:36:15 -0500

    feat: Server-Sent Event

--- handlers/routeHandlers.js
+++ handlers/routeHandlers.js
@@ -4,6 +4,7 @@ import { getData } from "../utils/getData.js";
 import { parseJSONBody } from "../utils/parseJSONBody.js";
 import { sanitizeInput } from "../utils/sanitizeInput.js";
 import { sendResponse } from "../utils/sendResponse.js";
+import { stories } from "../data/stories.js";
 
 export async function handleGet(res) {
   const data = await getData();
@@ -17,18 +18,42 @@ export async function handlePost(req, res) {
     const sanitizedBody = sanitizeInput(parsedBody);
     await addNewSighting(sanitizedBody);
 
-    /*
-    Challenge 2
-    1. At the top of this file, import the event emitter you have created.
-    2. Use it to emit a ‘sighting-added’ event.
-       What information does the listener function need?
-    3. Add a sighting to test!
-    */
-
     sightingEvents.emit("sighting-added", sanitizedBody);
 
     sendResponse(res, JSON.stringify(parsedBody), 201, "application/json");
   } catch (err) {
     sendResponse(res, JSON.stringify({ error: err }), 400, "application/json");
   }
+}
+
+export async function handleNews(req, res) {
+  res.statusCode = 200;
+
+  /*
+Challenge 1:
+  1. Set Content-Type, Cache-Control, and Connection headers
+*/
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Connection", "keep-alive");
+
+  setInterval(() => {
+    let randomIndex = Math.floor(Math.random() * stories.length);
+
+    /*
+Challenge 2:
+  1. Use res.write() to send an object to the frontend.
+
+  The object should include:
+    - an event property with a descriptive name.
+    - a story chosen at random from the stories array.
+
+  Remember, the object is contained in a string which starts with 'data: '.
+  What do you need at the end of the string to signal the end of a message block?
+*/
+
+    res.write(
+      `data: ${JSON.stringify({ event: "news-update", story: stories[randomIndex] })}\n\n`,
+    );
+  }, 3000);
 }
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import http from "node:http";
 import { serveStatic } from "./utils/serveStatic.js";
-import { handleGet, handlePost } from "./handlers/routeHandlers.js";
+import { handleGet, handleNews, handlePost } from "./handlers/routeHandlers.js";
 
 /*
 Challenge:
@@ -26,6 +26,8 @@ const server = http.createServer(async (req, res) => {
     } else if (req.method === "POST") {
       return await handlePost(req, res);
     }
+  } else if (req.url === "/api/news") {
+    return await handleNews(req, res);
   } else if (!req.url.startsWith("/api")) {
     return await serveStatic(__dirname, req, res);
   }
